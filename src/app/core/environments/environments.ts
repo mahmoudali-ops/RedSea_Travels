1adb0cfad391693379dd636740eb5a65e4a416a6
mahmoudali-ops <mahmoua487@gmail.com>
2025-12-09 16:11:51 +0200

    dfgdf

--- src/app/core/environments/environments.ts
+++ src/app/core/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    BaseUrl:'http://redseatravelseg.runasp.net',//'https://localhost:7065',
+    BaseUrl:'http://redseatravelseg.runasp.net',// 'https://localhost:7065',
     UrlServer:'http://localhost:4200'
  };
